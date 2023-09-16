0626b401b1c4bcf4082e27e9467b15c0b84788be
Alexandre <129619218+leefell@users.noreply.github.com>
2023-09-16 19:49:56 -0300

    Update classeMedia.cpp

--- Exercícios - Lista 1/Lista 1 - Ex 14 - 04 08 2023 - Media Ponderada Situacao/classeMedia.cpp
+++ Exercícios - Lista 1/Lista 1 - Ex 14 - 04 08 2023 - Media Ponderada Situacao/classeMedia.cpp
@@ -26,10 +26,10 @@ void classeMedia::lerNotas(){
     cin >> n2;
     cout << "Digite a terceira nota: ";
     cin >> n3;
-    cout << endl << "Codigo do Aluno: " << codigo;
+    cout << endl << "Codigo do Aluno: " << codigo << endl;
     if(this->calculo(n1,n2,n3) >= 5)
         cout << "Situacao: APROVADO." << endl;
-    else if(this->calculo(n1,n2,n3) < 5)
+    else 
         cout << "Situacao: REPROVADO." << endl;
 }
 
