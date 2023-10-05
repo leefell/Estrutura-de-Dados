caea589b03c016be8a6cf2bdf875b9c664b1650b
Alexandre <129619218+leefell@users.noreply.github.com>
2023-10-05 19:30:47 -0300

    Update cCadastro.h

--- Lista de Exercícios - Struct/Ex01/cCadastro.h
+++ Lista de Exercícios - Struct/Ex01/cCadastro.h
@@ -1,27 +1,26 @@
 #ifndef CCADASTRO_H
 #define CCADASTRO_H
-#include <string>
-
+#include <iostream>
 using namespace std;
-
 class cCadastro {
 public:
     
     struct sAluno{
+        
         string nome;
         string matricula;
         float n1, n2;
+        
     };
     
     cCadastro();
     cCadastro(const cCadastro& orig);
     virtual ~cCadastro();
     
-    //tanto no header quanto no cpp é preciso colocar * no começo ou [] no final ;
-    int cadastroAlunos();
-    void preencher(int qtd);
-    void mostrar(int qtd, sAluno *vetorAlunos);
-    float media(float n1, float n2);
+    void qtdAlunos();
+    void cadastrar(int qtd);
+    float media (float a, float b);
+    void imprimir(int qtd, sAluno *vetor);
     
 private:
 
