25c2659ead89311cc80b436a719eda5914b5a751
Alexandre <129619218+leefell@users.noreply.github.com>
2023-10-10 23:03:18 -0300

    Update PesquisaBinaria.cpp

--- Relevant Stuff/PesquisaBinaria.cpp
+++ Relevant Stuff/PesquisaBinaria.cpp
@@ -4,13 +4,34 @@ int PesquisaBinaria(int K[], int chave, int n){
     sup = n - 1;
     while(inf <= sup){
         meio = (inf + sup)/2;
-        if(chave == K[meio]){
+        if(chave == K[meio].cpf){
             return meio
-        }else if(chave < K[meio]){
+        }else if(chave < K[meio].cpf){
             sup = meio - 1;
         }else{
             inf = meio + 1;
         }
         return -1 /*não encontrar*/
     }
+}
+
+// Exemplo Prático
+
+void cCadastro::pesquisaBinaria(int qtd, sPessoa *vetor, string indice){
+    
+    int inf, sup, meio;
+    inf = 0;
+    sup = qtd - 1;
+    while(inf <= sup){
+        meio = (inf + sup)/2;
+        if(indice == vetor[meio].cpf){
+            cout << "O nome registrado a esse CPF e: " << vetor[meio].nome << endl;
+            return;
+        }else if(indice < vetor[meio].cpf){
+            sup = meio - 1;
+        }else{
+            inf = meio + 1;
+        }
+    }
+    cout << "CPF nao encontrado. " << endl;
 }
