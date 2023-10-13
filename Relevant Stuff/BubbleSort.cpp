101efee360143ae3f040960a1828839715faa1bf
Alexandre <129619218+leefell@users.noreply.github.com>
2023-10-13 19:41:04 -0300

    Update BubbleSort.cpp

--- Relevant Stuff/BubbleSort.cpp
+++ Relevant Stuff/BubbleSort.cpp
@@ -1,6 +1,6 @@
-void bubbleSort(Struct vetor[], int qtd){
+void bubbleSort(Struct *array, int qtd){
     
-    Struct temp;
+    Struct temp; // temporario
     for(int i = qtd - 1; i >= 1; i--){
         for(int j = 0; j < i; j++){
             if(vetor[j].nome > vetor[j+1].nome){
