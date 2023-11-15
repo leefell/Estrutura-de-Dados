e7634f54469ef305347ec865361cf5a3ae7809af
Alexandre <129619218+leefell@users.noreply.github.com>
2023-11-15 13:30:09 -0300

    Update pilha.cpp

--- Ponteiro/ListaEncadeada/pilha.cpp
+++ Ponteiro/ListaEncadeada/pilha.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 pilha::pilha() {
 
-    this->aux = NULL;
-    this->topo = NULL;
+    aux = NULL;
+    topo = NULL;
 
 }
 
@@ -16,15 +16,13 @@ pilha::~pilha() {
 }
 
 void pilha::menu() {
-    
-    // Tudo implementado
-    
+
     int opc;
     cout << "\n=========== MENU =========== " << endl;
-    cout << "[1] - Inserir valor à pilha " << endl;
-    cout << "[2] - Remover valor da pilha " << endl;
-    cout << "[3] - Mostrar valores da pilha" << endl;
-    cout << "[4] - Pesquisar valor da pilha " << endl;
+    cout << "[1] - Empilhar " << endl;
+    cout << "[2] - Desempilhar" << endl;
+    cout << "[3] - Mostrar valores da pilha " << endl;
+    cout << "[4] - Pesquisar valor na pilha " << endl;
     cout << "[0] - Sair" << endl;
     cout << "============================" << endl;
     cout << "Opcao: ";
@@ -32,75 +30,84 @@ void pilha::menu() {
 
     switch (opc) {
         case 1:
-            this->inserirValor();
+            this->empilhar();
             this->menu();
             break;
         case 2:
-            this->removerValor();
+            this->desempilhar();
             this->menu();
             break;
         case 3:
-            this->mostrarValores();
+            this->mostrar();
             this->menu();
             break;
         case 4:
-            this->pesquisarValor();
+            this->pesquisar();
             this->menu();
             break;
         case 0:
-            cout << "\nSaindo." << endl;
+            cout << "Saindo." << endl;
             break;
         default:
-            cout << "\nOpcao Invalida." << endl;
+            cout << "Opcao Invalida. Insira novamente" << endl;
             this->menu();
     }
+
 }
 
-bool pilha::vazio(no *structRecebido){
-    if(structRecebido == NULL)
-        return true;
-    else
-        return false;
-}
-
-void pilha::inserirValor() {
+void pilha::empilhar() {
 
     int x;
 
-    cout << "Digite o valor a ser empilhado: ";
-    this->aux = (struct no*) malloc(sizeof (aux));
+    cout << "Digite o valor para empilhar: ";
+    aux = (struct no*) malloc(sizeof (aux));
     cin >> x;
 
     this->aux->valor = x;
     this->aux->ant = this->topo;
     this->topo = this->aux;
+
 }
 
-void pilha::removerValor() {
+bool pilha::vazio(struct no *recebido) {
+
+    if (recebido == NULL)
+        return true;
+    else
+        return false;
+
+}
+
+void pilha::desempilhar() {
+
     if (!vazio(this->topo)) {
         this->aux = this->topo;
         this->topo = this->topo->ant;
+        cout << "\nO elemento desempilhado foi: " << this->aux->valor << endl;
         free(this->aux);
-        cout << "\nRemovido com sucesso!";
     }
 }
 
-void pilha::mostrarValores() {
+void pilha::mostrar() {
+
     no *p;
     p = this->topo;
     while (!vazio(p->ant)) {
-        cout << p->valor << endl;
+        cout << p -> valor << endl;
         p = p->ant;
     }
-    
-    cout << p->valor << endl;
 
-void pilha::pesquisarValor(){
+    cout << p->valor << endl;
+}
+
+void pilha::pesquisar() {
+
     int chave;
     cout << "Digite um valor para verificar se esta empilhado: ";
     cin >> chave;
 
-    for (this->aux = this->topo; this->aux != NULL; this->aux = this->aux->ant) {
+
+    for (aux = topo; aux != NULL; aux = aux->ant) {
         if (chave == this->aux->valor) {
             cout << "Valor esta empilhado." << endl;
         } else {
