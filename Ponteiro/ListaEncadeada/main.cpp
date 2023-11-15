40c3e39bf881a0a38550174ebfeab0ebd02134e7
Alexandre <129619218+leefell@users.noreply.github.com>
2023-11-15 13:28:34 -0300

    Update main.cpp

--- Ponteiro/ListaEncadeada/main.cpp
+++ Ponteiro/ListaEncadeada/main.cpp
@@ -1,20 +1,21 @@
 #include "fila.h"
 #include "pilha.h"
 #include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-   // fila obj;
-   // obj.menu();
-    
-    pilha obj;
-    obj.menu();
-    
+    char opc;
+    cout << "[F] - Fila" << endl;
+    cout << "[P] - Pilha" << endl;
+    cin >> opc;
+    if(opc == 'F' || opc == 'f'){
+        fila obj; obj.menu();
+    }
+    else if(opc == 'P' || opc == 'p'){
+        pilha obj; obj.menu();
+    }
+       
     return 0;
 }
-
