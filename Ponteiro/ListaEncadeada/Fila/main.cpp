085e42642ac2b8f3866ef8cb8999a0ea8673e636
leefell <alexandreaug21@gmail.com>
2023-11-18 17:17:04 -0300

    organizando

---
+++ Ponteiro/ListaEncadeada/Fila/main.cpp
@@ -0,0 +1,12 @@
+#include "fila.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    fila obj;
+    obj.menu();
+       
+    return 0;
+}
---
+++ Ponteiro/ListaEncadeada/Pilha/main.cpp
@@ -0,0 +1,12 @@
+#include "pilha.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    pilha obj;
+    obj.menu();
+       
+    return 0;
+}
