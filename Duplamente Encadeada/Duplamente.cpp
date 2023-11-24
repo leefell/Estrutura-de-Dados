596ea058e14d4e3fc9ec8717cf738edcb05339ad
Alexandre <129619218+leefell@users.noreply.github.com>
2023-11-24 11:31:46 -0300

    Lista Duplamente Encadeada

---
+++ Duplamente Encadeada/Duplamente.cpp
@@ -0,0 +1,118 @@
+#include "Duplamente.h"
+#include <iostream>
+using namespace std;
+
+Duplamente::Duplamente() {
+
+    inicio = NULL;
+    fim = NULL;
+    aux = NULL;
+
+}
+
+Duplamente::Duplamente(const Duplamente& orig) {
+}
+
+Duplamente::~Duplamente() {
+}
+
+void Duplamente::menu() {
+
+    int opc;
+    cout << "\n=========== MENU =========== " << endl;
+    cout << "[1] - Inserir valor à fila " << endl;
+    cout << "[2] - Remover valor da fila " << endl;
+    cout << "[3] - Mostrar valores da fila " << endl;
+    cout << "[0] - Sair " << endl;
+    cout << "============================" << endl;
+    cout << "Opcao: ";
+    cin >> opc;
+
+    switch (opc) {
+        case 1:
+            this->inserir();
+            this->menu();
+            break;
+        case 2:
+            this->remover();
+            this->menu();
+            break;
+        case 3:
+            this->mostrar();
+            this->menu();
+            break;
+        case 0:
+            cout << "Saindo" << endl;
+            break;
+
+    }
+}
+
+void Duplamente::inserir() {
+
+    int numero;
+    cout << "Digite o elemento para inserir: ";
+    cin >> numero;
+
+    this->aux = (struct no*) malloc(sizeof (aux));
+    this->aux->prox = NULL;
+    this->aux->ant = NULL;
+    this->aux->valor = numero;
+
+    if (this->inicio == NULL) {
+        this->inicio = this->aux;
+        this->inicio->prox = NULL;
+    } else {
+        this->fim->prox = aux;
+        this->aux->ant = fim;
+    }
+
+    this->fim = aux;
+    //this->inicio->ant = fim; // circular
+    //this->fim->prox = inicio; // circular
+
+}
+
+
+// Questao da prova -> remover um elemento qualquer do meio da lista duplamente encadeada
+//---------------------------------------------------------------------------------------
+
+no* Duplamente::pesquisar(int chave) {
+
+    for (this->aux = inicio; aux != NULL; this->aux = aux->prox) {
+        if (aux->valor == chave)
+            return aux;
+    }
+
+    return NULL;
+
+}
+
+void Duplamente::remover() {
+
+    int chave;
+    cout << "Digite o elemento que deseja remover: ";
+    cin >> chave;
+
+    aux = this->pesquisar(chave);
+    if (this->aux == this->inicio) {
+        this->inicio = this->inicio->prox;
+        this->inicio->ant = NULL;
+    } else if (this->aux == this->fim) {
+        this->fim = this->fim->ant;
+        this->fim->prox = NULL;
+    } else {
+        aux->ant->prox = aux->prox;
+        aux->prox->ant = aux->ant;
+    }
+    free(aux);
+}
+
+//---------------------------------------------------------------------------------------
+
+void Duplamente::mostrar() {
+
+    for (aux = inicio; aux != NULL; this->aux = this->aux->prox) {
+        cout << this->aux->valor << " ";
+    }
+}
---
+++ Duplamente Encadeada/main.cpp
@@ -0,0 +1,12 @@
+#include "Duplamente.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    Duplamente obj;
+    obj.menu();
+       
+    return 0;
+}
